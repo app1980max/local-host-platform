95864a601fe864b029393d8ba3cfeaa7ceea3816
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-15 19:36:03 +0200

    update

--- application/main.tf
+++ application/main.tf
@@ -16,34 +16,44 @@ provider "argocd" {
 }
 
 ###---Application
-resource "argocd_application" "grafana" {
-  depends_on = [helm_release.argocd]
+resource "argocd_application" "keda" {
   metadata {
-    name      = "grafana"
+    name      = "keda"
     namespace = "argocd"
   }
 
   spec {
     project = "default"
-
     source {
-      repo_url        = "https://github.com/bitnami/charts.git"
-      target_revision = "HEAD"
-      path            = "bitnami/grafana"
+      repo_url        = "https://kedacore.github.io/charts"
+      chart           = "keda"
+      target_revision = "2.17.0"
+      
+      helm {
+        values = <<EOF
+crds:
+  install: true
+EOF
+      }
     }
 
     destination {
       server    = "https://kubernetes.default.svc"
-      namespace = "default"
+      namespace = "keda"
     }
-
+    
     sync_policy {
       automated {
         prune = true
         self_heal = true
       }
+
+      sync_options = [
+        "CreateNamespace=true" # 👈 Enable namespace auto-creation
+      ]
     }
   }
+  depends_on = [argocd_application.minio]
 }
 
 
